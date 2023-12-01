0fb7b9218f196899c80eff32d60aecc3d59bc518
lDrakohh <lDrakohh@gmail.com>
2023-12-01 12:04:34 +0100

    feat: Añadidas valoraciones a propiedades.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from users.models import Profile, Alquiler
+from users.models import Profile, Alquiler, User
 
 # Create your models here.
 class Casa(models.Model):
@@ -14,3 +14,12 @@ class Casa(models.Model):
 
     def __str__(self):
         return self.titulo
+    
+class Comentario(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    casa = models.ForeignKey(Casa, on_delete=models.CASCADE)
+    texto = models.TextField(max_length=500)
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentario de {self.usuario.username} en {self.casa.titulo}"
